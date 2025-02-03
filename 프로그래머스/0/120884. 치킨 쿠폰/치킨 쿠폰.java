c1a369970cbe8bfd7112254926b35c555821f2a0
Min Joung Won <117150131+JWMin556@users.noreply.github.com>
2025-02-04 01:40:48 +0900

    [level 0] Title: 치킨 쿠폰, Time: 1.49 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int chicken) {
         int answer = 0; // 서비스로 받는 치킨의 수
-        while (chicken >= 10) {
+        while (chicken >= 10) { // 나는 이 문제에서 쿠폰이라는 존재를 생각하지 않고 풀었음
             chicken -= 10;
             answer++;
             chicken++;
