e4dcbe2207d83103d500a6836c0e48be2dd4b5ca
Min Joung Won <117150131+JWMin556@users.noreply.github.com>
2025-05-30 01:36:27 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.53 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,32 +2,39 @@ import java.util.*;
 
 class Solution {
     static int answer;
-    private static int dfs(Map<Integer, List<Integer>> graph, boolean[] visited, int currentNode, int n) {
+    
+    public static int dfs(Map<Integer, List<Integer>> tree, boolean[] visited, int cur, int n) {
         int count = 1;
-        visited[currentNode] = true;
+        visited[cur] = true;
         
-        for (int nextNode : graph.get(currentNode)) {
-            if (!visited[nextNode]) {
-                count += dfs(graph, visited, nextNode, n);
+        for (int next: tree.get(cur)) {
+            if (!visited[next]) {
+                count += dfs(tree, visited, next, n);
             }
         }
-        answer = Math.min(answer, Math.abs(n - 2*count));
+        
+        answer = Math.min(answer, Math.abs(n - count * 2));
         return count;
     }
     
     public int solution(int n, int[][] wires) {
         answer = n;
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        
+        // 먼저 일반적인 방식으로 그래프를 생성한다. 
+        Map<Integer, List<Integer>> tree = new HashMap<>();
         for (int i = 1; i <= n; i++) {
-            graph.put(i, new ArrayList<>());
-        }
-        for (int[] wire : wires) {
-            graph.get(wire[0]).add(wire[1]);
-            graph.get(wire[1]).add(wire[0]);
+            tree.put(i, new ArrayList<>());
         }
         
+        for (int[] wire : wires) {
+            tree.get(wire[0]).add(wire[1]);
+            tree.get(wire[1]).add(wire[0]);
+        }
+        
+        // dfs를 통해 특정 
         boolean[] visited = new boolean[n+1];
-        dfs(graph, visited, 1, n);
+        dfs(tree, visited, 1, n);
+        
         return answer;
     }
 }
