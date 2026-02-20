3e13a35c395dc744a1c96073b66c3eff14d1688c
Pocket Min <117150131+JWMin556@users.noreply.github.com>
2026-02-20 19:39:31 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main { 
+	public static void main(String[] args){
+        int a;
+        int b;
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
