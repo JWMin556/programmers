414fb603d53eb5116b4dbc68ac7db97254cbabcf
Pocket Min <117150131+JWMin556@users.noreply.github.com>
2026-02-24 17:34:01 +0900

    [level 2] Title: 주식가격, Time: 34.56 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 73.8 MB, 시간: 96.85 ms
+메모리: 77.1 MB, 시간: 34.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 19일 15:43:26
+2026년 02월 24일 17:33:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,41 +1,24 @@
-import java.util.ArrayDeque;
-import java.util.Iterator;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = {};
-        ArrayDeque<Integer> stack = new ArrayDeque<Integer>();  // 반환값을 보관하기 위한 스택배열
-        Queue<Integer> queue = new ArrayDeque<Integer>(); // 첫 prices들을 보관하기 위한 큐
-
-        for (int i = 0; i < prices.length; i++) {
-            queue.add(prices[i]);
-        }
-
-        while (!queue.isEmpty()) {
-            int price = queue.poll();
-            if (queue.size() == 0) {
-                stack.push(0);
+        int n = prices.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();  // 인덱스 저장용
+        
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
             }
-            int count = 1;
-            Iterator<Integer> iterator = queue.iterator();
-            while (iterator.hasNext()) {
-                int value = iterator.next();
-                if (price > value) {
-                    stack.push(count);
-                    break;
-                }
-                if (!iterator.hasNext()) {
-                    stack.push(count);
-                    break;
-                }
-                count++;
-            }
+            stack.push(i);
         }
-        answer = new int[stack.size()];
-        for (int i = answer.length - 1; i >= 0; i--) {
-            answer[i] = stack.pop();
+        
+        while(!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = n - 1 - index;
         }
+        
         return answer;
     }
 }
